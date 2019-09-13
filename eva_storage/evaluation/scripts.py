a6634f8cd97862bbe6f8fb1a3e86128272143718
Jaeho Bang <jaeho.bang@gmail.com>
2019-09-13 12:57:49 -0400

    Adding latest update of eva-storage

---
+++ eva_storage/evaluation/scripts.py
@@ -0,0 +1,11 @@
+"""
+We will put evaluation related scripts here
+Dump all the evaluation results to /eva/eva_storage/evaluation/results
+
+Evaluation TODOS:
+1. Analysis performance between all frames vs cluster representative frames (speed, accuracy)
+
+"""
+
+
+
