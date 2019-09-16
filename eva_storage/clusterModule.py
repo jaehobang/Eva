86fdd0e79d2696c6eb20c7c2ce5cbd671c46475c
Jaeho Bang <jaehob@andrew.cmu.edu>
2019-09-16 16:47:06 -0400

    Adding basic autoencoder and indexing module

--- eva_storage/clusterModule.py
+++ eva_storage/clusterModule.py
@@ -7,40 +7,40 @@ from sklearn.cluster import AgglomerativeClustering
 
 class ClusterModule:
 
-  def __init__(self):
-    self.ac = None
+    def __init__(self):
+        self.ac = None
 
-  def run(self, image_compressed, fps=20):
-    n_samples = len(image_compressed)
-    self.ac = AgglomerativeClustering(n_clusters=n_samples // fps)
-    start_time = time.time()
-    self.ac.fit(image_compressed)
-    print("Time to fit ", n_samples, ": ", time.time() - start_time)
-    return self.ac.labels_
+    def run(self, image_compressed, fps=20):
+        n_samples = len(image_compressed)
+        self.ac = AgglomerativeClustering(n_clusters=n_samples // fps)
+        start_time = time.time()
+        self.ac.fit(image_compressed)
+        print("Time to fit ", n_samples, ": ", time.time() - start_time)
+        return self.ac.labels_
 
-  def plot_distribution(self):
-    import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
-    axs.hist(self.ac.labels_, bins=max(self.ac.labels_) + 1)
-    plt.xlabel("Cluster Numbers")
-    plt.ylabel("Number of datapoints")
+    def plot_distribution(self):
+        import matplotlib.pyplot as plt
+        fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
+        axs.hist(self.ac.labels_, bins=max(self.ac.labels_) + 1)
+        plt.xlabel("Cluster Numbers")
+        plt.ylabel("Number of datapoints")
 
 
 
 if __name__ == "__main__":
-  from eva_storage.UNet import CAENetwork
-  from eva_storage.videoInputModule import VideoInputModule
-  import os
-  eva_dir = os.path.abspath('../')
-  detrac_dir = os.path.join(eva_dir, 'data', 'ua_detrac', 'small-data')
-  vim_ = VideoInputModule()
-  vim_.convert_video(detrac_dir)
-  image_table = vim_.get_image_array()
-  cae = CAENetwork()
-  cae.train(image_table)
+    from eva_storage.UNet import CAENetwork
+    from eva_storage.videoInputModule import VideoInputModule
+    import os
+    eva_dir = os.path.abspath('../')
+    detrac_dir = os.path.join(eva_dir, 'data', 'ua_detrac', 'small-data')
+    vim_ = VideoInputModule()
+    vim_.convert_video(detrac_dir)
+    image_table = vim_.get_image_array()
+    cae = CAENetwork()
+    cae.train(image_table)
 
-  images_compressed = cae.get_compressed(image_table)
+    images_compressed = cae.get_compressed(image_table)
 
-  cm = ClusterModule()
-  image_labels = cm.run(images_compressed)
-  cm.plot_distribution()
+    cm = ClusterModule()
+    image_labels = cm.run(images_compressed)
+    cm.plot_distribution()
