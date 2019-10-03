938e931250526bb49d06552e341af4c04c878694
Jaeho Bang <jaeho.bang@gmail.com>
2019-10-03 13:15:16 -0400

    Adding compression evaluation script

---
+++ eva_storage/evaluation/evaluate_compression.py
@@ -0,0 +1,60 @@
+"""
+This file is used to evaluate the compression method of the pipeline
+@Jaeho Bang
+"""
+
+
+import numpy as np
+from loaders.loader_uadetrac import LoaderUADetrac
+from eva_storage.preprocessingModule import PreprocessingModule
+from eva_storage.UNet import UNet
+from eva_storage.clusterModule import ClusterModule
+from filters.minimum_filter import FilterMinimum
+
+
+def get_rep_frames(images:np.ndarray, labels, image_cluster_labels):
+    visited_cluster_nums = set()
+    n_samples, height, width ,channels = images.shape
+    rep_images = np.ndarray(shape = (max(image_cluster_labels) + 1, height, width, channels))
+    rep_labels = np.ndarray(shape = (max(image_cluster_labels) + 1, height, width, channels))
+
+
+    for i in range(len(visited_cluster_nums)):
+        if image_cluster_labels[i] not in visited_cluster_nums:
+            visited_cluster_nums.add(image_cluster_labels)
+            rep_images[image_cluster_labels[i]] = images[i]
+            rep_labels[image_cluster_labels[i]] = labels[i]
+
+
+    return rep_images, rep_labels
+
+
+
+if __name__ == "__main__":
+    loader = LoaderUADetrac()
+    images = loader.load_cached_images()
+    labels = loader.load_cached_labels()
+    video_start_indices = loader.get_video_start_indices()
+    pm = PreprocessingModule()
+    seg_images = pm.run(images,video_start_indices)
+    unet = UNet()
+    unet.train(images, seg_images)
+    unet_compressed_images, unet_segmented_images = unet.execute()
+    cm = ClusterModule()
+    image_cluster_labels = cm.run(unet_compressed_images)
+
+    rep_images, rep_labels = get_rep_frames(images, labels['vehicle'], image_cluster_labels)
+    ## TODO: Chose the representative frames... now need to do evaluation with filters
+
+
+
+
+    # init a filter instance that is trained on all images
+    fm_everyframe = FilterMinimum()
+    fm_everyframe.train(images, labels['vehicle'])
+
+    fm_repframe = FilterMinimum()
+    fm_repframe.train(rep_images, rep_labels)
+
+
+
