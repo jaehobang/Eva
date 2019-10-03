861102f6320777a3902e98d7469d5bc1ec629f99
Jaeho Bang <jaehob@andrew.cmu.edu>
2019-10-03 15:09:46 -0400

    Made adjustments to filters; haven't tested yet

--- filters/models/ml_dnn.py
+++ filters/models/ml_dnn.py
@@ -11,34 +11,38 @@ from filters.models.ml_base import MLBase
 
 
 class MLMLP(MLBase):
-  def __init__(self, **kwargs):
-    super(MLMLP, self).__init__(MLBase)
-    if kwargs:
-      self.model = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(5, 2), random_state=1)
-    else:
-      self.model = MLPClassifier(**kwargs)
+    def __init__(self, **kwargs):
+        super(MLMLP, self).__init__()
+        if kwargs:
+            self.model = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                                   hidden_layer_sizes=(5, 2), random_state=1)
+        else:
+            self.model = MLPClassifier(**kwargs)
 
 
-  def train(self, X :np.ndarray, y :np.ndarray):
-    n_samples = X.shape[0]
-    division = int(n_samples * self.division_rate)
-    X_train = X[:division]
-    y_train = y[:division]
-    X_val = X[division:]
-    y_val = y[division:]
+    def train(self, X :np.ndarray, y :np.ndarray):
+        n_samples = X.shape[0]
+        X = X / 255.0
 
-    self.model.fit(X_train, y_train)
-    tic = time.time()
-    score = self.model.score(X_val, y_val)
-    toc = time.time()
-    val_samples = X_val.shape[0]
-    y_hat = self.model.predict(X_val)
+        division = int(n_samples * self.division_rate)
+        X_train = X[:division]
+        y_train = y[:division]
+        X_val = X[division:]
+        y_val = y[division:]
 
-    self.C = (toc - tic) / val_samples
-    self.A = score
-    self.R = 1 - float(sum(y_hat)) / len(y_hat)
+        self.model.fit(X_train, y_train)
+        tic = time.time()
+        score = self.model.score(X_val, y_val)
+        toc = time.time()
+        val_samples = X_val.shape[0]
+        y_hat = self.model.predict(X_val)
 
-  def predict(self, X :np.ndarray):
-    return self.model.predict(X)
+        self.C = (toc - tic) / val_samples
+        self.A = score
+        self.R = 1 - float(sum(y_hat)) / len(y_hat)
+
+    def predict(self, X :np.ndarray):
+        n_samples = X.shape[0]
+        X = X / 255.0
+        return self.model.predict(X)
 
