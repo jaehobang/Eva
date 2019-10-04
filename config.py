1cd3c3aa8dfe4b79a2a157d861a9ba05f1b50d9c
Jaeho Bang <jaehob@andrew.cmu.edu>
2019-10-04 14:32:09 -0400

    changed config to only use one gpu

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 import torch
 
 eva_dir = os.path.dirname(os.path.abspath(__file__))
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
 device_number = 0 # main gpu
+device = torch.cuda.device(device_number) if torch.cuda.is_available() else torch.device('cpu')
+
+
