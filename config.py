406277ab6603c321f36c823e9f9ab50826c2dd94
Jaeho Bang <jaeho.bang@gmail.com>
2019-10-07 13:07:41 -0400

    Committing config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 import torch
 
 eva_dir = os.path.dirname(os.path.abspath(__file__))
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device_number = 0 # main gpu
+device_number = 1 #alt GPU
+device = torch.cuda.device(device_number) if torch.cuda.is_available() else torch.device('cpu')
