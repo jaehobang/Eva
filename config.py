c63da16b0882cbbc40c44b9cf6e700fca318544a
Jaeho Bang <jaehob@andrew.cmu.edu>
2020-03-03 12:17:03 -0500

    Adding config and environment files

--- config.py
+++ config.py
@@ -3,7 +3,22 @@ import torch
 
 eva_dir = os.path.dirname(os.path.abspath(__file__))
 device_number = 0 # main gpu
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+train_device = torch.device('cuda:0')
+eval_device = train_device
+
+"""
+if torch.cuda.device_count() > 1:
+    train_device = torch.device('cuda:0')
+    eval_device = torch.device('cuda:1')
+elif torch.cuda.device_count() == 1:
+    train_device = torch.device('cuda:0')
+    eval_device = train_device
+else:
+    train_device = torch.device('cpu')
+    eval_device = torch.device('cpu')
+"""
 #device = torch.cuda.device(device_number) if torch.cuda.is_available() else torch.device('cpu')
 
 
