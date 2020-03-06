8c0957ed24e6e5b922ae937f0ee19a67018390ab
Jaeho Bang <jaeho.bang@gmail.com>
2020-03-06 16:51:50 -0500

    Adding config and loaders to pull from stream

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ import torch
 eva_dir = os.path.dirname(os.path.abspath(__file__))
 device_number = 0 # main gpu
 
+
+train_device = torch.device('cuda:0')
+eval_device = train_device
+
+"""
 if torch.cuda.device_count() > 1:
     train_device = torch.device('cuda:0')
     eval_device = torch.device('cuda:1')
@@ -13,7 +18,7 @@ elif torch.cuda.device_count() == 1:
 else:
     train_device = torch.device('cpu')
     eval_device = torch.device('cpu')
-
+"""
 #device = torch.cuda.device(device_number) if torch.cuda.is_available() else torch.device('cpu')
 
 
